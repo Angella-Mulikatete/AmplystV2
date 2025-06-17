ff8ed1d7073ac41cf8912e33aa3c722039692bd6
Angella-Mulikatete <mulikateteangella@gmail.com>
2025-06-17 17:39:37 +0300

    changed image path

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -9,6 +9,8 @@ import { RoleBasedRedirect } from "@/components/roleBasedRedirect";
 import { useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 import Onboarding from "@/components/onboarding";
+import heroImage from '@/assets/heroImage.jpeg';
+import logo from '@/assets/logo.png';
 
 const Index = () => {
   const navigate = useNavigate();
@@ -27,19 +29,12 @@ const Index = () => {
       {/* Navigation */}
        <nav className="sticky top-0 z-50 bg-white/80 backdrop-blur-md border-b border-gray-200">
         <div className="container mx-auto px-4 py-4 flex items-center justify-between">
-          {/* <div className="flex items-center space-x-2">
-            <div className="w-8 h-8 bg-gradient-to-r from-[#3A7CA5] to-[#88B04B] rounded-lg flex items-center justify-center">
-              <span className="text-white font-bold text-sm">A</span>
-            </div>
-            <span className="text-xl font-bold text-gray-900">Amplyst</span>
-          </div> */}
           <div className="flex items-center space-x-2">
             <img 
-              src="/assets/logo.png"  // or logo.png
+              src={logo}  // or logo.png
               alt="Amplyst Logo"
-              className="w-30 h-8 object-contain"  // maintains aspect ratio
+              className="w-30 h-6 object-contain"  // maintains aspect ratio
             />
-            {/* <span className="text-xl font-bold text-gray-900">Amplyst</span> */}
           </div>
           <div className="hidden md:flex items-center space-x-8">
             <a href="#features" className="text-gray-600 hover:text-[#3A7CA5] transition-colors">Features</a>
@@ -48,11 +43,6 @@ const Index = () => {
           </div>
           <div className="flex items-center space-x-4">
             <SignedOut>
-              {/* <SignInButton mode="modal">
-                <Button variant="ghost"  className="text-gray-600 hover:text-[#3A7CA5]">
-                  Sign In
-                </Button>
-              </SignInButton> */}
                <Button className="bg-gradient-to-r from-[#3A7CA5] to-[#88B04B] hover:from-[#3A7CA5]/90 hover:to-[#88B04B]/90" onClick={() => navigate("/register")}>
                   Get Started
                 </Button>
@@ -73,7 +63,7 @@ const Index = () => {
           {/* Left: Hero Image */}
           <div className="w-full lg:w-1/2 flex justify-center">
             <img
-               src="/assets/heroImage.jpeg" 
+               src={heroImage} 
               alt="Amplyst dashboard preview"
               className="w-full max-w-md object-contain scale-150"
               loading="lazy"
